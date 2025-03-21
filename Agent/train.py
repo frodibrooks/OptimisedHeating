c8059542bfa04dd57a638d5215bd93401e7bcbf8
frodibrooks <frodibrooks@gmail.com>
2025-03-21 11:57:05 +0000

    Þarf að gera nýtt conda enviroment með python 3.7 til al öll dependicy virki

--- Agent/train.py
+++ Agent/train.py
@@ -3,3 +3,11 @@ import os
 import glob
 import yaml
 import numpy as np 
+import pandas as pd
+import tensorflow as tf
+from stable_baselines.deepq.policies import FeedForwardPolicy
+from stable_baselines import DQN
+
+print("successfully imported all libraries")
+
+
