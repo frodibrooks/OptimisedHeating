9f2cb03bb8928efd43d593ed080d64d42b9f62cf
frodibrooks <frodibrooks@gmail.com>
2025-03-24 11:14:36 +0000

    Flest dependency komin og virka á conda ennv "dqn_env_python37"

--- Agent/train.py
+++ Agent/train.py
@@ -2,12 +2,16 @@ import argparse
 import os
 import glob
 import yaml
-import numpy as np 
+import numpy as np
 import pandas as pd
 import tensorflow as tf
 from stable_baselines.deepq.policies import FeedForwardPolicy
 from stable_baselines import DQN
+from stable_baselines.common.schedules import PiecewiseSchedule
+from pump_env import wds
+
 
 print("successfully imported all libraries")
+
 
 
