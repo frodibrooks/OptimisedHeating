ef6b45ca240138904263143cb0cb54f845f3b63e
frodibrooks <frodibrooks@gmail.com>
2025-03-24 11:17:26 +0000

    error cathcing when importing dependencies

--- Agent/train.py
+++ Agent/train.py
@@ -1,17 +1,22 @@
-import argparse
-import os
-import glob
-import yaml
-import numpy as np
-import pandas as pd
-import tensorflow as tf
-from stable_baselines.deepq.policies import FeedForwardPolicy
-from stable_baselines import DQN
-from stable_baselines.common.schedules import PiecewiseSchedule
-from pump_env import wds
+try:
+    import argparse
+    import os
+    import glob
+    import yaml
+    import numpy as np
+    import pandas as pd
+    import tensorflow as tf
+    from stable_baselines.deepq.policies import FeedForwardPolicy
+    from stable_baselines import DQN
+    from stable_baselines.common.schedules import PiecewiseSchedule
+    from pump_env import wds
 
 
-print("successfully imported all libraries")
+    print("successfully imported all libraries")
+except ImportError as e:
+    print("Import Error: ", e)
+    
+
 
 
 
