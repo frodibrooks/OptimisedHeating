40583368fc36acc898d37b49924719838eb2e536
frodibrooks <frodibrooks@gmail.com>
2025-05-07 10:56:34 +0200

    1000 langt train kinda að virka?? hahah

--- AgentV2/validate_agent.py
+++ AgentV2/validate_agent.py
@@ -27,7 +27,7 @@ action_dim = len(env.action_map)
 
 
 model = DQN(state_dim, action_dim)
-model.load_state_dict(torch.load("trained_model_vol10.pth"))
+model.load_state_dict(torch.load("trained_model_vol11.pth"))
 model.eval()
 
 # === Run validation ===
@@ -85,6 +85,6 @@ for timestep in range(env.episode_len):
 # === Save logs ===
 df = pd.DataFrame(full_logs)
 os.chdir(save_path)
-df.to_csv("validation_full_log_agent10.csv", index=False)
+df.to_csv("validation_full_log_agent11.csv", index=False)
 
-print("Validation complete. Results saved to validation_full_log_agent10.csv.")
+print("Validation complete. Results saved to validation_full_log_agent11.csv.")
