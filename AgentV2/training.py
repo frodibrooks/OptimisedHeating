ab38dbefe8268e503981f7b71659ce1ef78337be
frodibrooks <frodibrooks@gmail.com>
2025-05-27 14:50:51 +0200

    model 103

--- AgentV2/training.py
+++ AgentV2/training.py
@@ -98,16 +98,16 @@ if __name__ == "__main__":
     for episode in range(num_episodes):
 
         state = env.get_state()
-        print("Agent sees this: ",state[-8:])
+        # print("Agent sees this: ",state[-8:])
         demand = env.get_demand()
-        print("System demand is: ", demand[:5])
+        # print("System demand is: ", demand[:5])
         action_idx = agent.act(state)
-        print("Speed: ", env.action_map[action_idx])
+        # print("Speed: ", env.action_map[action_idx])
         next_state, reward, done, _ = env.step(action_idx,training=True)
 
-        print("New state: ",next_state[-8:])
-        print("Reward: ", reward)
-        print()
+        # print("New state: ",next_state[-8:])
+        # print("Reward: ", reward)
+        # print()
         agent.step(state, action_idx, reward, next_state, done)
         agent.decay_epsilon()
         state = next_state
