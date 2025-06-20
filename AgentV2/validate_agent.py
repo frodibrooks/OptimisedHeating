45a2fe7476c194678c489fa5d4118a0d8f544d6e
frodibrooks <frodibrooks@gmail.com>
2025-06-20 18:06:59 +0000

    decent

--- AgentV2/validate_agent.py
+++ AgentV2/validate_agent.py
@@ -30,8 +30,8 @@ os.chdir(program_dir)
 # demand_ptr = np.array([1,1,1,1,1,1]) # sumar
 
 
-# demand_ptr = np.array([0.8, 0.8, 0.9,0.9, 1.0,1.0, 1.1,1.1, 1.2,1.2, 1.3,1.3, 1.4,1.4])
-demand_ptr = np.array([1, 0.8, 0.8, 0.8,0.8, 0.8])
+demand_ptr = np.array([0.8, 0.8, 0.9,0.9, 1.0,1.0, 1.1,1.1, 1.2,1.2, 1.3,1.3, 1.4,1.4])
+# demand_ptr = np.array([1, 0.8, 0.8, 0.8,0.8, 0.8])
 # demand_ptr = np.array([1.3,1.3,1.3,1.4,1.4,1.4])
 
 
@@ -48,7 +48,7 @@ action_dim = len(env.action_map)
 
 
 model = DQN(state_dim, action_dim)
-model.load_state_dict(torch.load("trained_model_vol404.pth"))
+model.load_state_dict(torch.load("trained_model_vol405.pth"))
 model.eval()
 
 # === Run validation ===
@@ -130,4 +130,4 @@ for timestep in range(env.episode_len):
 # os.chdir(save_path)
 # df.to_csv("validation_full_log_agent404.csv", index=False)
 
-print("Validation complete. Results saved to validation_full_log_agent404.csv.")
+print("Validation complete. Results saved to validation_full_log_agent405.csv.")
